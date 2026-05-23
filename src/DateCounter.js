f1651b338ae7df25e0128d4b49e05a95abba45c5
Raj Shukla <rajshukla9630@gmail.com>
2026-05-23 11:20:37 +0530

    L-188 adding useReducer in datecounter prod

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,18 +1,45 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {count: 0, step: 1};
 
 function reducer(state, action){
-  console.log(state, action)
-  if(action.type === 'inc') return state + 1;
-  if(action.type === 'dec') return state - 1;
-  if(action.type === 'setCount') return action.payload;
+  console.log(state, action);
+
+  switch(action.type){
+    case "inc":
+      return {...state, count: state.count + state.step};
+    
+    case "dec": 
+      return {...state, count: state.count - state.step};
+    
+    case "setCount":
+      return {...state, count: action.payload};
+
+    case "setStep": 
+      return {...state, step: action.payload};
+
+    case "reset":
+      return {...initialState};
+      
+    default:
+      throw new Error('Unknown Action')
+  }
+
+
+  // if(action.type === 'inc') return state + 1;
+  // if(action.type === 'dec') return state - 1;
+  // if(action.type === 'setCount') return action.payload;
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
 
-  const [count,dispatch] = useReducer(reducer, 0);
+  // keeping both the states in one object using useReducer
+  const [state,dispatch] = useReducer(reducer, initialState); // changing name as state now includes both step and count
+  const {step, count}  = state;
 
-  const [step, setStep] = useState(1);
+
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -36,12 +63,14 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+    dispatch({type: 'setStep', payload: Number(e.target.value)})
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
+    dispatch({type:"reset"})
   };
 
   return (
